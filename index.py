8230fe8adc45b52158a8af34512a60b03308bbe3
lukaslion123 <luky.kozmon@gmail.com>
2025-03-06 16:42:51 +0100

    v0.0.1 - Doesn't really do anything, i was just figuring out how to do it + time constraints

---
+++ index.py
@@ -0,0 +1,8 @@
+import shapes
+import sim
+import sql
+
+
+def main():
+    val = input("enter number of grid spaces: ")
+    print("process started")
---
+++ shapes.py
@@ -0,0 +1,20 @@
+def generate_shapes(n):
+    arr = []
+    dict = {
+        1: 1,
+        2: 1,
+        3: 2,
+        4: 5,
+        5: 12,
+        6: 35,
+        7: 108,
+        8: 369,
+        9: 1285,
+        10: 4655
+    }
+    while arr.len() < dict(n):
+        shapearr = [(0,0)]
+        for i in range(n):
+            
+            
+            
